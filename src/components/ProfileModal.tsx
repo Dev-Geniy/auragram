37c2245f45af4d175f9158f08a87394fd05dbf09
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-14 00:31:32 +0300

    Update ProfileModal.tsx

--- src/components/ProfileModal.tsx
+++ src/components/ProfileModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { collection, query, where, orderBy, getDocs } from 'firebase/firestore';
 import { db } from '../services/firebase';

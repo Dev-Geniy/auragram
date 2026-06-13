8f7ddf546bb4d443007dd363fc9283b1305dd28f
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-13 12:55:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/auragram/', // Важно: указываем имя репозитория для GitHub Pages!
   plugins: [react()],
   resolve: {
     alias: {

5bc4962ecab4648d53e05ee695ba9723a3ffcdf3
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-13 15:55:16 +0300

    Add MarketPage component with placeholder text

---
+++ src/pages/MarketPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MarketPage() {
+  return (
+    <div className="p-8 text-gray-400">
+      Витрина B2B и маркетплейс в разработке...
+    </div>
+  );
+}

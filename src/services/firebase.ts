4d695f132c3b403f53e1a8dbf85de394d53741e2
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-13 12:49:22 +0300

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Сюда мы потом вставим реальные ключи от твоего проекта Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyChHiBebIPnMxRAV8Qc78GlmhIQDYGo61Y",
+  authDomain: "auragram-enterprise.firebaseapp.com",
+  projectId: "auragram-enterprise",
+  storageBucket: "auragram-enterprise.firebasestorage.app",
+  messagingSenderId: "1089884500236",
+  appId: "1:1089884500236:web:6b030acbca7628d543d929"
 };
 
 // Инициализация Firebase

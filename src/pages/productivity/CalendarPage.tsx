8b6ca3746c1a89d188ed3f00c3bd170756ba77c9
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-16 07:57:09 +0300

    Update CalendarPage.tsx

--- src/pages/productivity/CalendarPage.tsx
+++ src/pages/productivity/CalendarPage.tsx
@@ -1,1 +1,9 @@
-
+// Файл: src/pages/productivity/CalendarPage.tsx
+export default function CalendarPage() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">Календарь</h2>
+      {/* Здесь будет логика */}
+    </div>
+  );
+}

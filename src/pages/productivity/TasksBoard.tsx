95442e46169b0dbc5c648504dfdfb97dd6ba7ebf
Developer Evgeniy <79357491+Dev-Geniy@users.noreply.github.com>
2026-06-16 03:03:51 +0300

    Update TasksBoard.tsx

--- src/pages/productivity/TasksBoard.tsx
+++ src/pages/productivity/TasksBoard.tsx
@@ -1,1 +1,9 @@
-
+// Файл: src/pages/productivity/TasksBoard.tsx
+export default function TasksBoard() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold">Канбан-доска</h2>
+      {/* Здесь будет логика */}
+    </div>
+  );
+}
